d957d5b8d49909a9405355be788dcefe59bd5762
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-11 22:14:23 +0700

    Update page.tsx

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -3,12 +3,15 @@ import Container from "@/app/ components/Container";
 import ProductDetails from "./ProductDetails";
 import ListRating from "./ListRating";
 import { products } from "@/utils/products";
+import getProductById from "@/actions/getProductById";
+import NullData from "@/app/ components/NullData";
 interface IPrams{
     productId?: string;
 }
-const Product  = ({params} : {params:IPrams}) => {
-    console.log('params', params);
-    const product = products.find((item)=>item.id ==params.productId);
+const Product  = async({params} : {params:IPrams}) => {
+const product =await getProductById(params)
+if(!product) return<NullData title="Oops! Product with the given id does not exist"/>
+    
     return ( <div className="p-8">
         <Container>
             <ProductDetails product={product}/>
