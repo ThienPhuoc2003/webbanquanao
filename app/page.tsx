66bf6ca0aef720b228624e775c382b542c5ec578
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-11 22:14:24 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,28 @@ import Container from "./ components/Container";
 import { products } from "../utils/products";
 import { truncateText } from "@/utils/truncateText";
 import ProductCard from "./ components/products/ProductCard";
-export default function Home() {
+import getProducts, { IProductParams } from "@/actions/getProducts";
+import NullData from "./ components/NullData";
+
+interface HomeProps{
+  searchParams:IProductParams
+}
+export default async function Home({searchParams}:HomeProps) {
+  const products = await getProducts(searchParams)
+  if(products.length==0)
+  {
+    return <NullData title='Oops! No products found.Click "All" to clear filters'/>
+  }  
+  function shuffleArray(array: any)
+  {
+    for(let i = array.length-1;i>0;i--)
+    {
+      const j=Math.floor(Math.random()*(i+1));
+      [array[i],array[j]]=[array[j],array[i]]
+    }
+    return array
+  }
+  const shuffledProducts= shuffleArray(products)
   return (
     <div className="p-8">
       <Container>
@@ -13,7 +34,7 @@ export default function Home() {
           <div className="grid grid-cols-2 
           sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 
           2xl:grid-cols-6 gap:8" >
-            {products.map((product: any)  =>{
+            {shuffledProducts.map((product: any)  =>{
               return <ProductCard data={product}/>
             })}
           </div>
