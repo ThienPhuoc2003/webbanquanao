7e5d5d3bf14649539ec5fd7b6586e97d319e60c4
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-11 14:45:27 +0700

    Update ManageOrdersClient.tsx

--- app/admin/manage-orders/ManageOrdersClient.tsx
+++ app/admin/manage-orders/ManageOrdersClient.tsx
@@ -96,7 +96,7 @@ const ManageOrdersClient:React.FC<ManageOrdersClientProps> = ({orders}) => {
         return(<div className="flex justify-between gap-4 w-full">
             <ActionBtn icon={MdDeliveryDining} onClick={()=>{handleDispatch(params.row.id);}}/>
             <ActionBtn icon={MdDone} onClick={()=>{handleDeliver(params.row.id)}}/>
-            <ActionBtn icon={MdRemoveRedEye} onClick={()=>{router.push(`order/${params.row.id}`)}}/>
+            <ActionBtn icon={MdRemoveRedEye} onClick={()=>{router.push(`/order/${params.row.id}`)}}/>
             
             </div>);}
     }
