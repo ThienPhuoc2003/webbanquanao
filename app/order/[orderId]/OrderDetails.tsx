46cfb749ad524b28e4a1026146d61ba439a22d4e
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-12 00:35:42 +0700

    Update OrderDetails.tsx

--- app/order/[orderId]/OrderDetails.tsx
+++ app/order/[orderId]/OrderDetails.tsx
@@ -7,66 +7,71 @@ import Status from "@/app/ components/Status";
 import moment from "moment";
 import OrderItem from "./OrderItem";
 
-interface OrderDetailsProps{
-    order:Order;
-}
-
-const OrderDetails:React.FC<OrderDetailsProps> = ({order}) => {
-    
-    return ( 
-        <div className="max-w-[1150px] m-auto flex flex-col gap-2">
-            <div className="mt-8">
-                <Heading title="Order details"/>
-            </div>
-            <div>Order ID:{order.id}</div>
-            <div>
-                Total Amount:{""}
-                <span className="font-bold">{formatPrice(order.amount)}</span>
-            </div>
-            <div className="flex gap-2 items-center">
-                <div>Delivery status:</div>
-                <div>
-                    {order.deliveryStatus==="pending"?(
-                        <Status 
-                            text="pending"
-                            icon={MdAccessTimeFilled}
-                            bg="bg-slate-200"
-                            color="text-slate-700"
-                        />
-                    ):order.deliveryStatus==="dispatched"?(
-                        <Status 
-                            text="dispatched"
-                            icon={MdDeliveryDining}
-                            bg="bg-purple-200"
-                            color="text-purple-700"
-                        />
-                    ):order.deliveryStatus==="delivered"?(
-                        <Status 
-                            text="delivered"
-                            icon={MdDone}
-                            bg="bg-green-200"
-                            color="text-green-700"
-                        />
-                    ):(
-                        <></>
-                    )}
-                </div>
-            </div>
-          <div>Date:{moment(order.createDate).fromNow()}</div>
+interface OrderDetailsProps {
+    order: Order;
+  }
+  
+  const OrderDetails: React.FC<OrderDetailsProps> = ({ order }) => {
+    // Calculate the total amount
+    const totalAmount = order.products.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  
+    return (
+      <div className="max-w-[1150px] m-auto flex flex-col gap-2">
+        <div className="mt-8">
+          <Heading title="Order details" />
+        </div>
+        <div>Order ID: {order.id}</div>
+        <div>
+          Total Amount: <span className="font-bold">{formatPrice(totalAmount)}</span>
+        </div>
+        <div className="flex gap-2 items-center">
+          <div>Delivery status:</div>
           <div>
-            <h2 className="font-semibold mt-4 mb-2">Product ordered</h2>
-            <div className="grid grid-cols-5 text-sx gap-4 pb-2 items-center">
-                <div className="col-span-2 justify-self-start">PRODUCT</div>
-                <div className="justify-self-center">PRICE</div>
-                <div className="justify-self-center">QTY</div>
-                <div className="justify-self-end">TOTAL</div>
-            </div>
-            {order.products&& order.products.map((item)=>{
-                return <OrderItem key={item.id} item={item}></OrderItem>
-            })}
+            {order.deliveryStatus === "pending" ? (
+              <Status
+                text="pending"
+                icon={MdAccessTimeFilled}
+                bg="bg-slate-200"
+                color="text-slate-700"
+              />
+            ) : order.deliveryStatus === "dispatched" ? (
+              <Status
+                text="dispatched"
+                icon={MdDeliveryDining}
+                bg="bg-purple-200"
+                color="text-purple-700"
+              />
+            ) : order.deliveryStatus === "delivered" ? (
+              <Status
+                text="delivered"
+                icon={MdDone}
+                bg="bg-green-200"
+                color="text-green-700"
+              />
+            ) : (
+              <></>
+            )}
           </div>
         </div>
-     );
-}
- 
-export default OrderDetails;
+        <div>Date: {moment(order.createDate).fromNow()}</div>
+        <div>
+          <h2 className="font-semibold mt-4 mb-2">Product ordered</h2>
+          <div className="grid grid-cols-5 text-sx gap-4 pb-2 items-center">
+            <div className="col-span-2 justify-self-start">PRODUCT</div>
+            <div className="justify-self-center">PRICE</div>
+            <div className="justify-self-center">QTY</div>
+            <div className="justify-self-end">TOTAL</div>
+          </div>
+          {order.products &&
+            order.products.map((item) => {
+              return <OrderItem key={item.id} item={item}></OrderItem>;
+            })}
+        </div>
+      </div>
+    );
+  };
+  
+  export default OrderDetails;
