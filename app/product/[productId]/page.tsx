1303c446568ca78cae50b5b6c9569af25398dd2a
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-14 00:14:49 +0700

    Update page.tsx

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -5,18 +5,22 @@ import ListRating from "./ListRating";
 import { products } from "@/utils/products";
 import getProductById from "@/actions/getProductById";
 import NullData from "@/app/ components/NullData";
+import AddRating from "./AddRating";
+import { use } from "react";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 interface IPrams{
     productId?: string;
 }
 const Product  = async({params} : {params:IPrams}) => {
 const product =await getProductById(params)
+const user = await getCurrentUser()
 if(!product) return<NullData title="Oops! Product with the given id does not exist"/>
     
     return ( <div className="p-8">
         <Container>
             <ProductDetails product={product}/>
             <div className="flex flex-col mt-20 gap-4">
-                <div>Add Rating</div>
+                <AddRating product={product} user={user}/>
                 <ListRating product={product}/>
             </div>
         </Container>
