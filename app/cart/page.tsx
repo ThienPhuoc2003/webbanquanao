d2ed0f8546c4375535ff25d84e16baf8e8a9fcc5
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2024-03-24 14:37:12 +0700

    Update page.tsx
    SingleTon

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,14 +1,42 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import Container from "../ components/Container";
 import CartClient from "./CartClient";
+import { SafeUser } from "@/types";
 
-const Cart = async() => {
-    const currentUser = await getCurrentUser()
-    return ( <div className="pt-8">
-        <Container>
-            <CartClient currentUser= {currentUser} />
-        </Container>
-    </div> );
+class CurrentUserSingleton {
+    private static instance: CurrentUserSingleton | null = null;
+    private currentUser: SafeUser | null = null;
+
+  
+    static getInstance(): CurrentUserSingleton {
+        // Phương thức static này được sử dụng để lấy thể hiện duy nhất của lớp.
+        if (!CurrentUserSingleton.instance) {
+            CurrentUserSingleton.instance = new CurrentUserSingleton();
+        }
+        return CurrentUserSingleton.instance;
+    }
+
+    async getCurrentUser(): Promise<SafeUser | null> {
+        // Hàm này sẽ lấy thông tin người dùng nếu chưa có, sau đó trả về thông tin người dùng.
+        if (!this.currentUser) {
+            this.currentUser = await getCurrentUser();
+        }
+        return this.currentUser;
+    }
 }
- 
+
+const currentUserSingleton = CurrentUserSingleton.getInstance();
+
+const Cart = async () => {
+    const currentUser = await currentUserSingleton.getCurrentUser();
+
+    return (
+        <div className="pt-8">
+            <Container>
+                <CartClient currentUser={currentUser} />
+            </Container>
+        </div>
+    );
+}
+
 export default Cart;
